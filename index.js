90e32ca0cbe5e6e77d41babed7ff3db7ff31d62f
Olof Larsson <olarsson@live.com>
2020-11-05 10:18:51 +0100

    added example, added umd module pattern

--- index.js
+++ index.js
@@ -1,57 +1,80 @@
-const DEFAULTS = {
-  eventLimitCount: 4,
-  eventLimitTimeSpan: 200,
-};
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.returnExports = factory();
+  }
+}(typeof self !== 'undefined' ? self : this, function () {
 
-const MouseDetection = (attr) => {
+  // Just return a value to define the module export.
+  // This example returns an object, but the module
+  // can return a function as the exported value.
 
-  let self = {
+  const DEFAULTS = {
+    eventLimitCount: 4,
+    eventLimitTimeSpan: 200,
+  };
 
-    config: {
-      ...DEFAULTS,
-      ...attr
-    },
-    capturedEventsCount: [],
-    previousTime: Date.now(),
-    resolve: null,
+  const MouseDetection = (attr) => {
 
-    detectMouseMovementByIntensity() {
+    let self = {
 
-      const timeNow = Date.now();
+      config: {
+        ...DEFAULTS,
+        ...attr
+      },
+      capturedEventsCount: [],
+      previousTime: Date.now(),
+      resolve: null,
 
-      self.capturedEventsCount.push({
-        timePushed: timeNow
-      });
+      detectMouseMovementByIntensity() {
 
-      self.capturedEventsCount = self.capturedEventsCount.filter((item) => ((timeNow - item.timePushed) < self.config.eventLimitTimeSpan));
+        const timeNow = Date.now();
 
-      if (self.capturedEventsCount.length >= self.config.eventLimitCount) {
-        self.finalize();
+        self.capturedEventsCount.push({
+          timePushed: timeNow
+        });
+
+        self.capturedEventsCount = self.capturedEventsCount.filter((item) => ((timeNow - item.timePushed) < self.config.eventLimitTimeSpan));
+
+        if (self.capturedEventsCount.length >= self.config.eventLimitCount) {
+          self.finalize();
+        }
+
+      },
+
+      finalize() {
+        document.removeEventListener('mousemove', self.detectMouseMovementByIntensity);
+        self.resolve();
+      },
+
+      init(resolve) {
+        self.resolve = resolve;
+        document.addEventListener('mousemove', self.detectMouseMovementByIntensity);
       }
 
-    },
+    };
 
-    finalize() {
-      document.removeEventListener('mousemove', self.detectMouseMovementByIntensity);
-      self.resolve();
-    },
-
-    init(resolve) {
-      self.resolve = resolve;
-      document.addEventListener('mousemove', self.detectMouseMovementByIntensity);
-    }
+    return new Promise((resolve, reject) => {
+      self.init(resolve);
+    })
+      .then(
+        () => true, (error) => {
+          throw Error('An error occured:', error);
+        }
+      );
 
   };
 
-  return new Promise((resolve, reject) => {
-    self.init(resolve);
-  })
-    .then(
-      () => true, (error) => {
-        throw Error('An error occured:', error);
-      }
-    );
 
-};
+  return { MouseDetection };
 
-export { MouseDetection };
+
+}));
